1e903d08aa34a98bf08a46b82b419a30a196d00d
JbIPS <jeanbaptiste.richardet@gmail.com>
2015-07-01 21:40:17 +0200

    Use Forecast.io for weather forecasting
    Switch to The Dark Sky Forecast API v2 (close #1) for better data.
    Also add a license and a readme

---
+++ README.md
@@ -0,0 +1,30 @@
+# Huddy
+
+## Project Presentation
+
+This project aims to give you all the informations you'll need for you day in a glimpse: weather forecast, today events or traffic status.
+
+## How to run it
+
+First, you'll need to configure it with a `config.json` file at the root of the project.
+This file will look like
+```js
+{
+  "apiKey": "15ea3....", // Your Forecast.io API Key
+  "longitude": 2.29500,
+  "latitude": 48.85795,
+  "options": {
+    // Any options you want to pass to your query
+  }
+}
+```
+For more documentation on the Forecast API, see the [Dark Sky Forecast API](https://developer.forecast.io/docs/v2).
+
+Then you can just run
+```
+node index.js
+```
+
+## License
+
+This software is distributed with a MIT license. Feel free to share & modify it! PR are welcomed.
--- index.js
+++ index.js
@@ -1,87 +1,29 @@
 'use strict';
 
-var request = require('request');
-var moment = require('moment');
+var Forecast = require('forecast.io-bluebird');
+var config = require('./config');
 
-class WeatherChannel {
-  constructor(){
-  }
+var forecast = new Forecast({
+    key: config.apiKey
+});
 
-  static getPrevision(callback){
-    var options = {
-      url: 'http://www.infoclimat.fr/public-api/gfs/json',
-      port: 80,
-      method: 'GET',
-      qs: {
-        _ll: '48.8234808,2.3453789',
-        _auth: 'CBJQR1QqACJecwcwVSMBKFA4ATQMegIlA38EZwxpVCkJYlEwVjZSNFY4WyYFKldhAy5UNwA7VWUBagZ+XS8CYwhiUDxUPwBnXjEHYlV6ASpQfgFgDCwCJQNnBGsMf1Q2CWNRMVYrUjFWP1sxBStXYQMzVDQAIFVyAWMGZV0zAmQIb1A0VDUAZV44B2dVegEqUGYBZAw7Am8DaARhDGNUPwlvUTJWN1IxVmhbPQUrV2EDMFQ9AD9VbQFkBmBdMAJ+CHRQTVREAH9ecQcnVTABc1B+ATQMbQJu',
-        _c: '6bc0b4c2f1bd4aed9d10eabe2eff953e'
-      }
-    };
+var time = new Date('2015-07-01T07:00:00.000Z').getTime() / 1000;
 
-    request(options, function (err, response, body) {
-      if(err)
-        return callback(err);
-
-      var previsions = JSON.parse(body);
-      if(previsions.request_state != 200){
-        callback(new Error('Error while getting infos from API: '
-          +previsions.message));
-      }
-      else{
-        delete previsions.request_state;
-        delete previsions.request_key;
-        delete previsions.message;
-        delete previsions.model_run;
-        delete previsions.source;
-        callback(null, previsions);
-      }
-    });
-  }
-}
-
-function getAveragePrevision(previsions) {
-  var tempSum = 0, windSum = 0, rainSum = 0, snow = false;
-  var length = Object.keys(previsions).length;
-  for(let key in previsions){
-    let prevision = previsions[key];
-    console.log(key, prevision.temperature['2m']-273.15);
-    console.log(prevision);
-    tempSum += prevision.temperature['2m'];
-    windSum += prevision.vent_moyen['10m'];
-    rainSum += prevision.pluie;
-    if(!snow && prevision.risque_neige === 'oui')
-      snow = true;
-  }
-
-  return {
-    temperature: tempSum/length,
-    wind: windSum/length,
-    rain: rainSum/length,
-    snow: snow
-  }
-}
-
-WeatherChannel.getPrevision(function (err, previsions) {
-  if(err)
-    console.error(err);
-  else {
-    var now = moment();
-    var midday = now.set({'hour': 12, 'minute': 0, 'second': 0});
-    var morning = {};
-    var afternoon = {};
-    for(let time of Object.keys(previsions)){
-      var date = moment(time, 'YYYY-MM-DD HH:mm:ss');
-      if(date.date() === now.date()){
-        if(date < midday)
-          morning[time] = previsions[time];
-        else
-          afternoon[time] = previsions[time];
-      }
-    }
-
-    var morningAv = getAveragePrevision(morning);
-    var afternoonAv = getAveragePrevision(afternoon);
-    console.log(morningAv.temperature-273.15, afternoonAv.temperature-273.15);
-  }
+forecast.fetch(config.latitude, config.longitude, config.options)
+.then(function(result) {
+    let todayReport = result.daily.data[0];
+    let date = new Date(todayReport.time*1000);
+    console.info('==============================');
+    console.info('Report for '+
+      date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear());
+    console.info(todayReport.summary);
+    console.info(todayReport.temperatureMin + '(' +
+      todayReport.apparentTemperatureMin+') / ' +
+      todayReport.temperatureMax + '(' +
+      todayReport.apparentTemperatureMax + ')');
+    console.info('Rain probability: ' + todayReport.precipProbability+'%');
+    console.info('Winds: ' + todayReport.windSpeed+'m/s');
+})
+.catch(function(error) {
+    console.error(error);
 });
